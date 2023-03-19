cf2fda44ea49ec31d5f1606ff828e01557c72885
Ajayi Elizabeth Oluwabukola <69092379+Deniss444@users.noreply.github.com>
2023-03-19 04:21:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -37,13 +37,13 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['919074692450','']
-global.premium = ['919074692450','919074692450']
+global.owner = ['2347051364291','']
+global.premium = ['2347051364291','2347051364291']
 global.botname = 'ABHISHEK-SER'
-global.ownername = 'Abhishek Suresh'
+global.ownername = 'Zaddy Tech'
 global.group1 = 'https://chat.whatsapp.com/B7QtwpUd2RMAVQ57KzosBj'
 global.group2 = 'https://chat.whatsapp.com/B7QtwpUd2RMAVQ57KzosBj'
-global.packname = 'ABHISHEK-SER'
+global.packname = 'Zaddy-Tech'
 global.ytchannel = 'https://youtube.com/@comedymelodych8468'
 global.monayawal = '100000'
 global.darahawal = '100'
